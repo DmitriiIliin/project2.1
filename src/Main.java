7a6545c07f54b21e1536bbb98bd7872a970f36f2
Dmitrii Iliin <dmitryilyin@mail.ru>
2023-02-22 22:01:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 20; //бонусов за 1 милю
+        int b = 13676; // стоимость билета
+        int c; // количество бонусных миль за перелеты
+        c = b / a;
+        //Количество бонусных миль
+        System.out.println((c) + " Ваше количество бонусных миль" );
+    }
+
+}
